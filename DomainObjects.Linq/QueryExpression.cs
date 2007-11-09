23aeeb0e61099902e02bcedecc92ce67c03972a5
Matthias Goertler <matthias.goertler@rubicon.eu>
2007-11-09 14:58:02 +0000

    QueryExpression: FromClause And JoinClause added

---
+++ DomainObjects.Linq/QueryExpression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Rubicon.Utilities;
+
+namespace Rubicon.Data.DomainObjects.Linq
+{
+  public class QueryExpression
+  {
+    private readonly FromClause _fromClause;
+    private readonly QueryBody _queryBody;
+
+    public QueryExpression (FromClause from, QueryBody queryBody)
+    {
+      ArgumentUtility.CheckNotNull ("from", from);
+      ArgumentUtility.CheckNotNull ("queryBody", queryBody);
+
+      _fromClause = from;
+      _queryBody = queryBody;
+    }
+
+    public FromClause FromClause
+    {
+      get { return _fromClause; }
+    }
+
+    public QueryBody QueryBody
+    {
+      get { return _queryBody; }
+    }
+  }
+}
