7b53a3d5714f82105ac2f18316a3d70e74fce6e4
Matthias Goertler <matthias.goertler@rubicon.eu>
2007-12-10 10:29:34 +0000

    Set up test environment by referencing Rubicon.Data.DomainObjects.UnitTests for using the same test database

---
+++ DomainObjects.Linq.UnitTests/QueryExecutorTest.cs
@@ -0,0 +1,13 @@
+using System;
+using NUnit.Framework;
+using Rubicon.Data.DomainObjects.UnitTests;
+using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
+
+namespace Rubicon.Data.DomainObjects.Linq.UnitTests
+{
+  [TestFixture]
+  public class QueryExecutorTest : ClientTransactionBaseTest
+  {
+    
+  }
+}
---
+++ DomainObjects.Linq.UnitTests/SetupFixture.cs
@@ -0,0 +1,11 @@
+using System;
+using NUnit.Framework;
+
+namespace Rubicon.Data.DomainObjects.Linq.UnitTests
+{
+  [SetUpFixture]
+  public class SetupFixture : Rubicon.Data.DomainObjects.UnitTests.SetUpFixture
+  {
+    
+  }
+}
