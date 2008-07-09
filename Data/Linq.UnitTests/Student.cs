46560f9f31b237d97d49cb4ab5014e2575fab545
Matthias Goertler <matthias.goertler@rubicon.eu>
2008-07-09 11:17:44 +0000

    added license header (to .cs files)

--- Data/Linq.UnitTests/Student.cs
+++ Data/Linq.UnitTests/Student.cs
@@ -1,3 +1,13 @@
+/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the re:motion license agreement in license.txt. If you did not 
+ * receive it, please visit http://www.re-motion.org/licensing.
+ * 
+ * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ */
+
 using System.Collections.Generic;
 
 namespace Remotion.Data.Linq.UnitTests
