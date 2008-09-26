9e75b305190114cd5bd42951e97e4b9a4e7f873b
Fabian Schmied <fabian.schmied@rubicon.eu>
2008-09-26 10:33:57 +0000

    Updated assembly versions in AssemblyInfos

--- Data/Linq/Properties/AssemblyInfo.cs
+++ Data/Linq/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.11.0.2")]
+[assembly: AssemblyVersion("1.11.4.2")]
