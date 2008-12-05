bd79538e328dcf73c782073900e758e8d5497807
Michael Ketting <michael.ketting@rubicon.eu>
2008-12-05 21:46:37 +0000

    Updated license header

--- Data/Linq/Properties/AssemblyInfo.cs
+++ Data/Linq/Properties/AssemblyInfo.cs
@@ -1,13 +1,18 @@
-/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
- *
- * This program is free software: you can redistribute it and/or modify it under 
- * the terms of the re:motion license agreement in license.txt. If you did not 
- * receive it, please visit http://www.re-motion.org/licensing.
- * 
- * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
- */
-
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2008 rubicon informationstechnologie gmbh, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+//
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+//
 using System;
 using System.Reflection;
 
---
+++ license/replace-licenseheader.ps1
@@ -0,0 +1,55 @@
+param
+(
+  [string]$rootPath = @( throw "Please specify a rootPath." ),
+  [string]$logFile = @( throw "Please specify a logFile." )
+)
+
+
+filter replace-licenseHeader([string]$pathToLicenseHeaderFile, [string]$pathToOldLicenseHeaderFile)
+{
+  [string]$tempFile = 'c:\temp.txt'
+  [string]$pathToSourceFile = $_.FullName
+  
+  $licenseHeader = get-content $pathToLicenseHeaderFile
+  $oldLicenseHeader = get-content $pathToOldLicenseHeaderFile
+  $source = get-content $pathToSourceFile
+    
+  if ($source.Length -lt $licenseHeader.Length)
+  {
+    $error = "File $_ already has a license header different from the old license header. Source file contains less lines than license file."
+    write-host $error
+    add-content -path $logFile -value $error
+    return
+  }
+
+  for ($i = 0; $i -lt $oldLicenseHeader.Length; $i++)
+  {
+    if ($source[$i] -eq $oldLicenseHeader[$i])
+    {
+      $source[$i] = $null;
+    }
+    else
+    {
+      $error = "File $_ already has a license header different from the old license header. Difference at line $($i+1)."
+      write-host $error
+      add-content -path $logFile -value $error
+      return
+    }
+  }
+  
+  if ($source.Length -ge $oldLicenseHeader.Length -and $source[$oldLicenseHeader.Length].Length -eq 0)
+  {
+    $source[$oldLicenseHeader.Length] = $null;
+  }
+
+  set-content -path $tempFile -value $licenseHeader
+  add-content -path $tempFile -value $source
+  
+  write-host Replacing license header to file $_.FullName
+  move-item $tempFile $pathToSourceFile -force
+}
+
+get-ChildItem -path $rootPath -include '*.cs' -exclude 'prereq\*' -recurse | replace-licenseHeader 'licenseHeader.cs.txt' 'licenseHeader_old.cs.txt'
+#get-ChildItem -path $rootPath -include '*.js' -exclude 'prereq\*' -recurse | replace-licenseHeader 'licenseHeader.cs.txt'
+#get-ChildItem -path $rootPath -include '*.aspx' -exclude 'prereq\*' -recurse | replace-licenseHeader 'licenseHeader.asx.txt'
+#get-ChildItem -path $rootPath -include '*.ascx' -exclude 'prereq\*' -recurse | replace-licenseHeader 'licenseHeader.asx.txt'
