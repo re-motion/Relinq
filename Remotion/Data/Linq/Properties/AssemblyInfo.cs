830280120bff045b1c9039550412aed54974fbc8
Michael Ketting <michael.ketting@rubicon.eu>
2008-12-28 15:29:13 +0000

    Added support for product specific assembly info entries.

--- Remotion/Data/Linq/Properties/AssemblyInfo.cs
+++ Remotion/Data/Linq/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Reflection;
 [assembly: AssemblyDescription("Framework for working with linq queries.")]
 [assembly: AssemblyConfiguration(".NET Framework: net-3.5, build type: debug")]
 [assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
-[assembly: AssemblyProduct("Remotion")]
+[assembly: AssemblyProduct("re-motion core framework (www.re-motion.org)")]
 [assembly: AssemblyCopyright("(c) 2005 - 2008 rubicon informationstechnologie gmbh, www.rubicon.eu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -43,4 +43,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.11.14.2")]
+[assembly: AssemblyVersion("1.11.19.2")]
---
+++ Remotion/Data/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2008 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Reflection;
+using Remotion.Development.Data.UnitTesting.DomainObjects.Linq;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+
+[assembly: AssemblyTitle("Unit Tests for Remotion DomainObjects Library")]
+[assembly: AssemblyDescription("Unit Tests for Remotion DomainObjects Library")]
+[assembly: AssemblyConfiguration(".NET Framework: net-3.5, build type: debug")]
+[assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
+[assembly: AssemblyProduct("re-motion core framework (www.re-motion.org)")]
+[assembly: AssemblyCopyright("(c) 2005 - 2008 rubicon informationstechnologie gmbh, www.rubicon.eu")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+[assembly: CLSCompliant(true)]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.11.19.2")]
+
+[assembly: ApplyQueryExecutorMixin]
