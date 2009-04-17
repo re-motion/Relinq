c2e495632cc1f5dddc4b0fb50e20cbb2342f9f8f
Michael Ketting <michael.ketting@rubicon.eu>
2009-04-17 13:14:20 +0000

    Fixed typo in license headers.

--- Remotion/Data/UnitTests/Linq/ExtensionMethodsTest.cs
+++ Remotion/Data/UnitTests/Linq/ExtensionMethodsTest.cs
@@ -5,13 +5,13 @@
 // and/or modify it under the terms of the GNU Lesser General Public License 
 // version 3.0 as published by the Free Software Foundation.
 // 
-// This framework is distributed in the hope that it will be useful, 
+// re-motion is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of 
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public License
-// along with this framework; if not, see http://www.gnu.org/licenses.
+// along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
 using System.Collections.Generic;
