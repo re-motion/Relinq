f258f171279f56e134678fba547fa50f0701e4ea
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-06-04 10:16:46 +0000

    Added documentation for QueryParser (RM-1186)

--- Remotion/Data/Linq/Parsing/Structure/QueryParser.cs
+++ Remotion/Data/Linq/Parsing/Structure/QueryParser.cs
@@ -15,6 +15,7 @@
 // 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Remotion.Data.Linq.Clauses;
 using Remotion.Data.Linq.Parsing.Structure.IntermediateModel;
@@ -22,15 +23,32 @@ using Remotion.Utilities;
 
 namespace Remotion.Data.Linq.Parsing.Structure
 {
+  /// <summary>
+  /// Takes an <see cref="Expression"/> tree and parses it into a <see cref="QueryModel"/> by use of an <see cref="ExpressionTreeParser"/>.
+  /// It first transforms the <see cref="Expression"/> tree into a chain of <see cref="IExpressionNode"/> instances, and then calls 
+  /// <see cref="IExpressionNode.CreateClause"/> to instantiate the <see cref="IClause"/>s. With those, a <see cref="QueryModel"/> is
+  /// created and returned.
+  /// </summary>
   public class QueryParser
   {
     private readonly ExpressionTreeParser _expressionTreeParser;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueryParser"/> class, using a default instance of <see cref="ExpressionTreeParser"/> to
+    /// convert <see cref="Expression"/> instances into <see cref="IExpressionNode"/>s. The <see cref="MethodCallExpressionNodeTypeRegistry"/> 
+    /// used has all relevant methods of the <see cref="Queryable"/> class automatically 
+    /// registered.
+    /// </summary>
     public QueryParser ()
         : this (new ExpressionTreeParser (MethodCallExpressionNodeTypeRegistry.CreateDefault()))
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueryParser"/> class, using the given <paramref name="expressionTreeParser"/> to
+    /// convert <see cref="Expression"/> instances into <see cref="IExpressionNode"/>s.
+    /// </summary>
+    /// <param name="expressionTreeParser">The expression tree parser.</param>
     public QueryParser (ExpressionTreeParser expressionTreeParser)
     {
       ArgumentUtility.CheckNotNull ("expressionTreeParser", expressionTreeParser);
@@ -42,6 +60,11 @@ namespace Remotion.Data.Linq.Parsing.Structure
       get { return _expressionTreeParser; }
     }
 
+    /// <summary>
+    /// Gets the <see cref="QueryModel"/> of the given <paramref name="expressionTreeRoot"/>.
+    /// </summary>
+    /// <param name="expressionTreeRoot">The expression tree to parse.</param>
+    /// <returns>A <see cref="QueryModel"/> that represents the query defined in <paramref name="expressionTreeRoot"/>.</returns>
     public QueryModel GetParsedQuery (Expression expressionTreeRoot)
     {
       ArgumentUtility.CheckNotNull ("expressionTreeRoot", expressionTreeRoot);
@@ -53,25 +76,40 @@ namespace Remotion.Data.Linq.Parsing.Structure
       // TODO: After COMMONS-1178, this code will not be needed any longer.
       var bodyClauses = new List<IBodyClause>();
       var findMainFromClause = FindMainFromClause (selectClause, bodyClauses);
+      
       var queryModel = new QueryModel (expressionTreeRoot.Type, findMainFromClause, selectClause);
       foreach (var bodyClause in bodyClauses)
-      {
         queryModel.AddBodyClause (bodyClause);
-      }
+
       return queryModel;
     }
 
+    /// <summary>
+    /// Recursively creates <see cref="IClause"/>s from a chain of <see cref="IExpressionNode"/>s, hooking up the clauses in the same
+    /// order as the corresponding nodes.
+    /// </summary>
+    /// <param name="node">The last node in the chain to process.</param>
+    /// <returns>An <see cref="IClause"/> that corresponds to <paramref name="node"/>. The chain defined by <see cref="IExpressionNode.Source"/>
+    /// is reflected in the chain defined by <see cref="IClause.PreviousClause"/>.</returns>
     private IClause CreateClauseChain (IExpressionNode node)
     {
-      if (node.Source == null)
+      if (node.Source == null) // this is the end of the node chain, create a clause symbolizing the end of the clause chain
         return node.CreateClause (null);
       else
-      {
+      { // this is not the end of the cahin, process the rest of the chain before processing this node
         var previousClause = CreateClauseChain (node.Source);
         return node.CreateClause (previousClause);
       }
     }
 
+    /// <summary>
+    /// Gets or create the <see cref="SelectClause"/> marking the end of the clause chain.
+    /// </summary>
+    /// <param name="lastNode">The last node in the chain of <see cref="IExpressionNode"/>s.</param>
+    /// <param name="lastClause">The last clause produced by the chain defined by <paramref name="lastNode"/>.</param>
+    /// <returns><paramref name="lastClause"/> if it is a <see cref="SelectClause"/>, or a new <see cref="SelectClause"/> that references
+    /// <paramref name="lastClause"/> as its <see cref="SelectClause.PreviousClause"/>. If a new <see cref="SelectClause"/> is created, its selector
+    /// will take the data streamed out by <paramref name="lastClause"/> and return it unchanged.</returns>
     private SelectClause GetOrCreateSelectClause (IExpressionNode lastNode, IClause lastClause)
     {
       if (lastClause is SelectClause)
