d75cf88c0268d62a9d25c39ff3fe64b72e7f3d99
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-06-05 11:53:03 +0000

    added integration tests for QueryParser (SelectMany/Let)

--- Remotion/Data/UnitTests/Linq/Parsing/Structure/QueryParserIntegrationTest.cs
+++ Remotion/Data/UnitTests/Linq/Parsing/Structure/QueryParserIntegrationTest.cs
@@ -38,6 +38,16 @@ namespace Remotion.Data.UnitTests.Linq.Parsing.Structure
     }
 
     [Test]
+    public void SimpleSelect ()
+    {
+      var expression = SelectTestQueryGenerator.CreateSimpleQuery_SelectExpression (_querySource);
+      var queryModel = _queryParser.GetParsedQuery (expression);
+
+      Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
+      Assert.That (queryModel.SelectOrGroupClause.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
+    }
+
+    [Test]
     public void SimpleWhere ()
     {
       var query = WhereTestQueryGenerator.CreateSimpleWhereQuery (_querySource);
@@ -59,14 +69,14 @@ namespace Remotion.Data.UnitTests.Linq.Parsing.Structure
     {
       var query = WhereTestQueryGenerator.CreateMultiWhereQuery (_querySource);
       var queryModel = _queryParser.GetParsedQuery (query.Expression);
-      
+
       var selectClause = (SelectClause) queryModel.SelectOrGroupClause;
       var whereClause1 = (WhereClause) queryModel.BodyClauses[0];
       var whereClause2 = (WhereClause) queryModel.BodyClauses[1];
       var whereClause3 = (WhereClause) queryModel.BodyClauses[2];
-      
+
       Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
-      
+
       Assert.That (queryModel.BodyClauses.Count, Is.EqualTo (3));
       Assert.That (whereClause1.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
       Assert.That (whereClause2.PreviousClause, Is.EqualTo (whereClause1));
@@ -88,8 +98,65 @@ namespace Remotion.Data.UnitTests.Linq.Parsing.Structure
       Assert.That (selectClause.PreviousClause, Is.EqualTo (whereClause));
       Assert.That (whereClause.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
 
-      var operand = ((UnaryExpression)((MethodCallExpression)expression).Arguments[1]).Operand;
+      var operand = ((UnaryExpression) ((MethodCallExpression) expression).Arguments[1]).Operand;
       Assert.That (whereClause.Predicate, Is.SameAs (operand));
+    }
+
+    [Test]
+    public void GeneralSelectMany ()
+    {
+      var query = FromTestQueryGenerator.CreateMultiFromQuery (_querySource, _querySource);
+      var queryModel = _queryParser.GetParsedQuery (query.Expression);
+
+      Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
+      Assert.That (queryModel.BodyClauses[0], Is.InstanceOfType (typeof (MemberFromClause)));
+      var memberFromClause = (MemberFromClause) queryModel.BodyClauses[0];
+
+      Assert.That (queryModel.SelectOrGroupClause.PreviousClause, Is.EqualTo (memberFromClause));
+      Assert.That (memberFromClause.Identifier.Name, Is.EqualTo ("s2"));
+    }
+
+    [Test]
+    public void WhereSelectMany ()
+    {
+      var expression = MixedTestQueryGenerator.CreateReverseFromWhere_WhereExpression (_querySource, _querySource);
+      var queryModel = _queryParser.GetParsedQuery (expression);
+
+      Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
+      var whereClause = (WhereClause) queryModel.BodyClauses[0];
+      var memberFromClause = (MemberFromClause) queryModel.BodyClauses[1];
+      Assert.That (queryModel.SelectOrGroupClause.PreviousClause, Is.EqualTo (memberFromClause));
+      Assert.That (whereClause.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
+    }
+
+    [Test]
+    public void RecursiveSelectMany ()
+    {
+      var expression = MixedTestQueryGenerator.CreateThreeFromWhereQuery_SelectManyExpression (_querySource, _querySource, _querySource);
+      var queryModel = _queryParser.GetParsedQuery (expression);
+
+      var memberFromClause1 = (MemberFromClause) queryModel.BodyClauses[0];
+      var whereClause = (WhereClause) queryModel.BodyClauses[1];
+      var memberFromClause2 = (MemberFromClause) queryModel.BodyClauses[2];
+
+      Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
+      Assert.That (memberFromClause1.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
+      Assert.That (whereClause.PreviousClause, Is.EqualTo (memberFromClause1));
+      Assert.That (memberFromClause2.PreviousClause, Is.EqualTo (whereClause));
+      Assert.That (queryModel.SelectOrGroupClause.PreviousClause, Is.EqualTo (memberFromClause2));
+    }
+
+    [Test]
+    public void Let ()
+    {
+      var expression = LetTestQueryGenerator.CreateSimpleSelect_LetExpression (_querySource);
+      var queryModel = _queryParser.GetParsedQuery (expression);
+
+      var letClause = (LetClause) queryModel.BodyClauses[0];
+
+      Assert.That (queryModel.MainFromClause.PreviousClause, Is.Null);
+      Assert.That (letClause.PreviousClause, Is.EqualTo (queryModel.MainFromClause));
+      Assert.That (queryModel.SelectOrGroupClause.PreviousClause, Is.EqualTo (letClause));
     }
   }
 }
