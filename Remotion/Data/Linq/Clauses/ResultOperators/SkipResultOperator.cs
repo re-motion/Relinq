2c01faf7eaf6c8f3de29f9a44ff3f1e53c0aef9f
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-07-20 13:18:21 +0000

    added SkipExpressionNode added SkipResultOperator (RM-1313)

---
+++ Remotion/Data/Linq/Clauses/ResultOperators/SkipResultOperator.cs
@@ -0,0 +1,119 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Clauses.ExecutionStrategies;
+using Remotion.Data.Linq.Clauses.ExpressionTreeVisitors;
+using Remotion.Utilities;
+
+namespace Remotion.Data.Linq.Clauses.ResultOperators
+{
+  /// <summary>
+  /// Represents the skip part of a query. This is a result operator, operating on the whole result set of a query.
+  /// </summary>
+  /// <example>
+  /// In C#, the "skip" clause in the following example corresponds to a <see cref="SkipResultOperator"/>.
+  /// <code>
+  /// var query = (from s in Students
+  ///              select s).Skip(3);
+  /// </code>
+  /// </example>
+  public class SkipResultOperator : ResultOperatorBase
+  {
+    private Expression _count;
+
+    public SkipResultOperator (Expression count)
+      : base (CollectionExecutionStrategy.Instance)
+    {
+      ArgumentUtility.CheckNotNull ("count", count);
+      Count = count;
+    }
+
+    public Expression Count
+    {
+      get { return _count; }
+      set
+      {
+        ArgumentUtility.CheckNotNull ("value", value);
+        if (value.Type != typeof (int))
+        {
+          var message = string.Format ("The value expression returns '{0}', an expression returning 'System.Int32' was expected.", value.Type);
+          throw new ArgumentException (message, "value");
+        }
+
+        _count = value;
+      }
+    }
+
+    /// <summary>
+    /// Gets the constant <see cref="int"/> value of the <see cref="Count"/> property, assuming it is a <see cref="ConstantExpression"/>. If it is
+    /// not, an expression is thrown.
+    /// </summary>
+    /// <returns>The constant <see cref="int"/> value of the <see cref="Count"/> property.</returns>
+    public int GetConstantCount ()
+    {
+      var countAsConstantExpression = Count as ConstantExpression;
+      if (countAsConstantExpression != null)
+      {
+        return (int) countAsConstantExpression.Value;
+      }
+      else
+      {
+        var message = string.Format (
+            "Count ('{0}') is no ConstantExpression, it is a {1}.",
+            FormattingExpressionTreeVisitor.Format (Count),
+            Count.GetType ().Name);
+        throw new InvalidOperationException (message);
+      }
+    }
+
+    public override ResultOperatorBase Clone (CloneContext cloneContext)
+    {
+      return new SkipResultOperator (Count);
+    }
+
+    public override object ExecuteInMemory (object input)
+    {
+      ArgumentUtility.CheckNotNull ("input", input);
+      return InvokeGenericOnEnumerable<IEnumerable<object>> (input, ExecuteInMemory);
+    }
+
+    public IEnumerable<T> ExecuteInMemory<T> (IEnumerable<T> input)
+    {
+      ArgumentUtility.CheckNotNull ("input", input);
+
+      var count = GetConstantCount ();
+      return input.Skip (count);
+    }
+
+    public override Type GetResultType (Type inputResultType)
+    {
+      ArgumentUtility.CheckNotNull ("inputResultType", inputResultType);
+      ReflectionUtility.GetItemTypeOfIEnumerable (inputResultType, "inputResultType");
+
+      return inputResultType;
+    }
+
+    public override string ToString ()
+    {
+      return "Skip(" + Count + ")";
+    }
+
+    
+  }
+}
---
+++ Remotion/Data/UnitTests/Linq/Clauses/ResultOperators/SkipResultOperatorTest.cs
@@ -0,0 +1,100 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Clauses;
+using Remotion.Data.Linq.Clauses.ExecutionStrategies;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.Clauses.ResultOperators;
+using Remotion.Data.UnitTests.Linq.TestDomain;
+using Remotion.Utilities;
+
+namespace Remotion.Data.UnitTests.Linq.Clauses.ResultOperators
+{
+  [TestFixture]
+  public class SkipResultOperatorTest
+  {
+    private SkipResultOperator _resultOperator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _resultOperator = new SkipResultOperator (Expression.Constant (2));
+    }
+
+    [Test]
+    [ExpectedException (typeof (ArgumentException))]
+    public void Initialization_NoIntExpression ()
+    {
+      new SkipResultOperator (Expression.Constant ("12"));
+    }
+
+    [Test]
+    public void Clone ()
+    {
+      var clonedClauseMapping = new QuerySourceMapping ();
+      var cloneContext = new CloneContext (clonedClauseMapping);
+      var clone = _resultOperator.Clone (cloneContext);
+
+      Assert.That (clone, Is.InstanceOfType (typeof (SkipResultOperator)));
+    }
+
+    [Test]
+    public void ExecuteInMemory ()
+    {
+      var items = new[] { 1, 2, 3, 0, 2 };
+      var result = _resultOperator.ExecuteInMemory (items);
+
+      Assert.That (result.ToArray (), Is.EqualTo (new[] { 3, 0, 2 }));
+    }
+
+    [Test]
+    public void ExecutionStrategy ()
+    {
+      Assert.That (_resultOperator.ExecutionStrategy, Is.SameAs (CollectionExecutionStrategy.Instance));
+    }
+
+    [Test]
+    public void GetConstantCount ()
+    {
+      Assert.That (_resultOperator.GetConstantCount (), Is.EqualTo (2));
+    }
+
+    [Test]
+    [ExpectedException (typeof (InvalidOperationException))]
+    public void GetConstantCount_NoConstantExpression ()
+    {
+      var resultOperator = new SkipResultOperator (new QuerySourceReferenceExpression (ExpressionHelper.CreateMainFromClause ()));
+      resultOperator.GetConstantCount ();
+    }
+
+    [Test]
+    public void GetResultType ()
+    {
+      Assert.That (_resultOperator.GetResultType (typeof (IQueryable<Student>)), Is.SameAs (typeof (IQueryable<Student>)));
+    }
+
+    [Test]
+    [ExpectedException (typeof (ArgumentTypeException))]
+    public void GetResultType_InvalidType ()
+    {
+      _resultOperator.GetResultType (typeof (Student));
+    }
+  }
+}
