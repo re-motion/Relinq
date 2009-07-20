1328642916b1be6d109a416d9e9286f4b144c602
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-07-20 09:58:52 +0000

    added LongCountExpressionNode added LongCountResultOperator (without implementation) (RM-1313)

---
+++ Remotion/Data/UnitTests/Linq/Parsing/Structure/IntermediateModel/LongCountExpressionNodeTest.cs
@@ -0,0 +1,60 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Remotion.Data.Linq.Clauses.ResultOperators;
+using Remotion.Data.Linq.Parsing.Structure.IntermediateModel;
+
+namespace Remotion.Data.UnitTests.Linq.Parsing.Structure.IntermediateModel
+{
+  [TestFixture]
+  public class LongLongCountExpressionNodeTest : ExpressionNodeTestBase
+  {
+    private LongCountExpressionNode _node;
+
+    public override void SetUp ()
+    {
+      base.SetUp ();
+      _node = new LongCountExpressionNode (CreateParseInfo (), null);
+    }
+
+    [Test]
+    public void SupportedMethod_WithoutPredicate ()
+    {
+      AssertSupportedMethod_Generic (LongCountExpressionNode.SupportedMethods, q => q.LongCount (), e => e.LongCount ());
+    }
+
+    [Test]
+    public void SupportedMethod_WithPredicate ()
+    {
+      AssertSupportedMethod_Generic (LongCountExpressionNode.SupportedMethods, q => q.LongCount (o => o == null), e => e.LongCount (o => o == null));
+    }
+
+    [Test]
+    [ExpectedException (typeof (InvalidOperationException))]
+    public void Resolve_ThrowsInvalidOperationException ()
+    {
+      _node.Resolve (ExpressionHelper.CreateParameterExpression (), ExpressionHelper.CreateExpression (), ClauseGenerationContext);
+    }
+
+    [Test]
+    public void Apply ()
+    {
+      TestApply (_node, typeof (LongCountResultOperator));
+    }
+  }
+}
