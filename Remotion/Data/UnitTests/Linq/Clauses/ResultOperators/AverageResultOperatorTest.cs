18704f88c2ac9c3bb3b5ada2b4d73ade69118516
Michael Ketting <michael.ketting@rubicon.eu>
2009-11-04 08:56:07 +0000

    temp-commit: Updated license headers to LGPL v2.1.

--- Remotion/Data/UnitTests/Linq/Clauses/ResultOperators/AverageResultOperatorTest.cs
+++ Remotion/Data/UnitTests/Linq/Clauses/ResultOperators/AverageResultOperatorTest.cs
@@ -3,7 +3,8 @@
 // 
 // The re-motion Core Framework is free software; you can redistribute it 
 // and/or modify it under the terms of the GNU Lesser General Public License 
-// version 3.0 as published by the Free Software Foundation.
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
 // 
 // re-motion is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of 
