fbb7cec2f3659479ee4c4431dc91ef5422d64894
Michael Ketting <michael.ketting@rubicon.eu>
2009-12-03 08:47:45 +0000

    Code cleanup after moving ObservableCollection to re-linq. (RM-1950)

--- Remotion/Data/Linq/Collections/ChangeResistantObservableCollectionEnumerator.cs
+++ Remotion/Data/Linq/Collections/ChangeResistantObservableCollectionEnumerator.cs
@@ -17,7 +17,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Remotion.Utilities;
+using Remotion.Data.Linq.Utilities;
 
 namespace Remotion.Data.Linq.Collections
 {
