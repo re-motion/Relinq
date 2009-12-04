905b22475e9ef15412e02fefe09547d79ae12a8d
Fabian Schmied <fabian.schmied@rubicon.eu>
2009-12-04 13:32:08 +0000

    Cleaned up

--- Remotion/Data/Linq/Clauses/ResultOperators/AllResultOperator.cs
+++ Remotion/Data/Linq/Clauses/ResultOperators/AllResultOperator.cs
@@ -15,7 +15,6 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Remotion.Data.Linq.Clauses.ExpressionTreeVisitors;
