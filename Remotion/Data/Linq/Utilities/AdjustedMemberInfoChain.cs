4a8fa827c1971d7e61f75efea331b821034830b7
Matthias Goertler <matthias.goertler@rubicon.eu>
2009-12-30 09:42:06 +0000

    added new class for adjusted MemberInfo chain (RM-2010)

---
+++ Remotion/Data/Linq/Utilities/AdjustedMemberInfoChain.cs
@@ -0,0 +1,33 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Reflection;
+
+namespace Remotion.Data.Linq.Utilities
+{
+  public struct AdjustedMemberInfoChain
+  {
+    public AdjustedMemberInfoChain (MemberInfo accessedMember, MemberInfo[] joinedMembers) : this()
+    {
+      AccessedMember = accessedMember;
+      JoinedMembers = joinedMembers;
+    }
+
+    public MemberInfo AccessedMember { get; private set; }
+    public MemberInfo[] JoinedMembers { get; private set; }
+  }
+}
