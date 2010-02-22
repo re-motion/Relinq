331bea8971597bf66e5aa5b723911ac7adbe2156
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-02-22 13:54:17 +0000

    added SqlStatement which represents the sql backend model added SqlQueryModelVisitor to transform QueryModel to SqlStatement added FromExpression to SqlStatement added SelectProjection to SqlStatement (RM-2315)

---
+++ Remotion/Data/Linq/SqlBackend/SqlStatementModel/SelectProjection.cs
@@ -0,0 +1,56 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq.Expressions;
+using Remotion.Data.Linq.Clauses;
+using Remotion.Data.Linq.Clauses.Expressions;
+using Remotion.Data.Linq.Parsing;
+using Remotion.Data.Linq.Utilities;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel
+{
+  /// <summary>
+  /// <see cref="SelectProjection"/> holds expression of select part of an expression query.
+  /// </summary>
+  public class SelectProjection : ExtensionExpression
+  {
+    private Expression _expression;
+
+    public SelectProjection (Type type, Expression expression)
+        : base (type)
+    {
+      ArgumentUtility.CheckNotNull ("expression", expression);
+      _expression = expression;
+    }
+
+    public Expression Expression
+    {
+      get { return _expression; }
+      set { _expression = ArgumentUtility.CheckNotNull ("value", value); }
+    }
+
+    protected internal override Expression VisitChildren (ExpressionTreeVisitor visitor)
+    {
+      return this;
+    }
+
+    public override Expression Accept (ExpressionTreeVisitor visitor)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Remotion/Data/Linq/SqlBackend/SqlStatementModel/SqlQueryModelVisitor.cs
@@ -0,0 +1,50 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Data.Linq.Clauses;
+
+namespace Remotion.Data.Linq.SqlBackend.SqlStatementModel
+{
+  /// <summary>
+  /// <see cref="SqlQueryModelVisitor"/> generates a <see cref="SqlStatement"/> from a query model.
+  /// </summary>
+  public class SqlQueryModelVisitor : QueryModelVisitorBase
+  {
+    private SqlStatement _sqlStatement;
+
+    public SqlQueryModelVisitor ()
+    {
+      _sqlStatement = new SqlStatement();
+    }
+
+    public SqlStatement SqlStatement
+    {
+      get { return _sqlStatement; }
+    }
+
+    public override void VisitSelectClause (SelectClause selectClause, QueryModel queryModel)
+    {
+      _sqlStatement.SelectProjection = new SelectProjection (selectClause.GetType (), selectClause.Selector);
+    }
+
+    public override void VisitMainFromClause (MainFromClause fromClause, QueryModel queryModel)
+    {
+      _sqlStatement.FromExpression = new FromExpression (fromClause.ItemType, fromClause.FromExpression);
+    }
+
+  }
+}
