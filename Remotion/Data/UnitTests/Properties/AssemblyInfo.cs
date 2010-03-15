98fc1a1501eb3d2066e9d8d55973a8330abb0ee2
Michael Ketting <michael.ketting@rubicon.eu>
2010-03-15 07:07:40 +0000

    Updated AssemblyInfo.cs

--- Remotion/Data/UnitTests/Properties/AssemblyInfo.cs
+++ Remotion/Data/UnitTests/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using Remotion.Development.Data.UnitTesting.DomainObjects.Linq;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.13.41.2")]
+[assembly: AssemblyVersion("1.13.50.2")]
 
 [assembly: ApplyQueryExecutorMixin]
