73f07b750dd29a28d3ead6844bb595e0868368ae
Michael Ketting <michael.ketting@rubicon.eu>
2010-03-29 05:21:53 +0000

    Updated AssemblyInfo.cs

--- Remotion/Data/Linq/Properties/AssemblyInfo.cs
+++ Remotion/Data/Linq/Properties/AssemblyInfo.cs
@@ -44,4 +44,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.13.50.2")]
+[assembly: AssemblyVersion("1.13.52.2")]
