899687ae6b4a145c71a356f64e322bf2648e648b
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-06-09 13:17:56 +0000

    added MediumTrustQueryParserIntegrationTest (RM-2812)

---
+++ Remotion/Data/Linq.UnitTests/Linq/Core/Parsing/Structure/QueryParserIntegrationTests/MediumTrustQueryParserIntegrationTest.cs
@@ -0,0 +1,50 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Linq;
+using System.Security.Permissions;
+using NUnit.Framework;
+using Remotion.Data.Linq.Sandboxing;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.Core.Parsing.Structure.QueryParserIntegrationTests
+{
+  [TestFixture]
+  public class MediumTrustQueryParserIntegrationTest
+  {
+    [Test]
+    public void MediumTrust ()
+    {
+      var mediumTrust = PermissionSets.GetMediumTrust (AppDomain.CurrentDomain.BaseDirectory, Environment.MachineName);
+      var permissions = mediumTrust.Concat (new[] { new ReflectionPermission (ReflectionPermissionFlag.MemberAccess) }).ToArray ();
+
+      var types = (from t in typeof (MediumTrustQueryParserIntegrationTest).Assembly.GetTypes ()
+                   where t.Namespace == typeof (MediumTrustQueryParserIntegrationTest).Namespace
+                       && t != typeof (MediumTrustQueryParserIntegrationTest)
+                       && !t.IsAbstract && !t.IsNested
+                   select t).ToArray ();
+
+      var testFixtureResults = SandboxTestRunner.RunTestFixturesInSandbox (
+          types,
+          permissions,
+          new[] { typeof (MediumTrustQueryParserIntegrationTest).Assembly }); // TODO: Remove full trust when NUnit supports partially trusted callers
+      var testResults = testFixtureResults.SelectMany (r => r.TestResults);
+
+      foreach (var testResult in testResults)
+        testResult.EnsureNotFailed ();
+    }
+  }
+}
