127588328f1dbe7a5d39ea8356fed42a2ff4428d
Andreas Obkircher <andreas.obkircher@rubicon.eu>
2010-07-01 10:46:02 +0000

    added TupleExpressionBuilder moved tuple methods from SqlPreparationSubStatementTableFactory to TupleExpressionBuilder (RM-2974)

---
+++ Remotion/Data/Linq.UnitTests/Linq/Core/Parsing/TupleExpressionBuilderTest.cs
@@ -0,0 +1,170 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.Linq.Parsing;
+
+namespace Remotion.Data.Linq.UnitTests.Linq.Core.Parsing
+{
+  [TestFixture]
+  public class TupleExpressionBuilderTest
+  {
+    [Test]
+    public void AggregateExpressionsIntoTuple_OneExpression ()
+    {
+      var expression1 = Expression.Constant ("expr1");
+      var tupleExpression = TupleExpressionBuilder.AggregateExpressionsIntoTuple (new[] { expression1 });
+
+      Assert.That (tupleExpression, Is.SameAs (expression1));
+    }
+
+    [Test]
+    public void AggregateExpressionsIntoTuple_TwoExpressions ()
+    {
+      var expression1 = Expression.Constant ("expr1");
+      var expression2 = Expression.Constant ("expr2");
+
+      var result = TupleExpressionBuilder.AggregateExpressionsIntoTuple (new[] { expression1, expression2 });
+
+      var tupleCtor = typeof (KeyValuePair<string, string>).GetConstructor (new[] { typeof (string), typeof (string) });
+      var expectedResult = Expression.New (
+          tupleCtor,
+          new[] { expression1, expression2 },
+          tupleCtor.DeclaringType.GetMethod ("get_Key"),
+          tupleCtor.DeclaringType.GetMethod ("get_Value"));
+
+      ExpressionTreeComparer.CheckAreEqualTrees (result, expectedResult);
+    }
+
+    [Test]
+    public void AggregateExpressionsIntoTuple_ThreeeExpressions ()
+    {
+      var expression1 = Expression.Constant ("expr1");
+      var expression2 = Expression.Constant ("expr2");
+      var expression3 = Expression.Constant ("expr3");
+
+      var result = TupleExpressionBuilder.AggregateExpressionsIntoTuple (new[] { expression1, expression2, expression3 });
+
+      var innerTupleCtor = typeof (KeyValuePair<string, string>).GetConstructor (new[] { typeof (string), typeof (string) });
+      var outerTupleCtor =
+          typeof (KeyValuePair<string, KeyValuePair<string, string>>).GetConstructor (
+              new[] { typeof (string), typeof (KeyValuePair<string, string>) });
+
+      var innerTupleKeyGetter = innerTupleCtor.DeclaringType.GetMethod ("get_Key");
+      var innerTupleValueGetter = innerTupleCtor.DeclaringType.GetMethod ("get_Value");
+      var outerTupleKeyGetter = outerTupleCtor.DeclaringType.GetMethod ("get_Key");
+      var outerTupleValueGetter = outerTupleCtor.DeclaringType.GetMethod ("get_Value");
+
+      var expectedResult = Expression.New (
+          outerTupleCtor,
+          new Expression[]
+          {
+              expression1,
+              Expression.New (innerTupleCtor, new[] { expression2, expression3 }, innerTupleKeyGetter, innerTupleValueGetter)
+          },
+          outerTupleKeyGetter,
+          outerTupleValueGetter);
+
+      ExpressionTreeComparer.CheckAreEqualTrees (result, expectedResult);
+    }
+
+    [Test]
+    public void GetExpressionsFromTuple_OneExpression ()
+    {
+      var expression = Expression.Constant ("expr1");
+      
+      var result = TupleExpressionBuilder.GetExpressionsFromTuple (expression);
+
+      var enumerator = result.GetEnumerator ();
+      enumerator.MoveNext ();
+      Assert.That (enumerator.Current, Is.SameAs (expression));
+    }
+
+    [Test]
+    public void GetExpressionsFromTuple_TwoExpressions ()
+    {
+      var expression1 = Expression.Constant ("expr1");
+      var expression2 = Expression.Constant ("expr2");
+      var tupleCtor = typeof (KeyValuePair<string, string>).GetConstructor (new[] { typeof (string), typeof (string) });
+      var tupleExpression = Expression.New (
+          tupleCtor,
+          new[] { expression1, expression2 },
+          tupleCtor.DeclaringType.GetMethod ("get_Key"),
+          tupleCtor.DeclaringType.GetMethod ("get_Value"));
+
+      var result = TupleExpressionBuilder.GetExpressionsFromTuple (tupleExpression);
+
+      var enumerator = result.GetEnumerator();
+      enumerator.MoveNext();
+      ExpressionTreeComparer.CheckAreEqualTrees (
+          enumerator.Current, MemberExpression.MakeMemberAccess (tupleExpression, tupleCtor.DeclaringType.GetProperty ("Key")));
+      enumerator.MoveNext();
+      ExpressionTreeComparer.CheckAreEqualTrees (
+          enumerator.Current, MemberExpression.MakeMemberAccess (tupleExpression, tupleCtor.DeclaringType.GetProperty ("Value")));
+    }
+
+    [Test]
+    public void GetExpressionsFromTuple_ThreeExpressions ()
+    {
+      var expression1 = Expression.Constant ("expr1");
+      var expression2 = Expression.Constant ("expr2");
+      var expression3 = Expression.Constant ("expr3");
+
+      var innerTupleCtor = typeof (KeyValuePair<string, string>).GetConstructor (new[] { typeof (string), typeof (string) });
+      var outerTupleCtor =
+          typeof (KeyValuePair<string, KeyValuePair<string, string>>).GetConstructor (
+              new[] { typeof (string), typeof (KeyValuePair<string, string>) });
+
+      var innerTupleKeyGetter = innerTupleCtor.DeclaringType.GetMethod ("get_Key");
+      var innerTupleValueGetter = innerTupleCtor.DeclaringType.GetMethod ("get_Value");
+      var outerTupleKeyGetter = outerTupleCtor.DeclaringType.GetMethod ("get_Key");
+      var outerTupleValueGetter = outerTupleCtor.DeclaringType.GetMethod ("get_Value");
+
+      var tupleExpression = Expression.New (
+          outerTupleCtor,
+          new Expression[]
+          {
+              expression1,
+              Expression.New (innerTupleCtor, new[] { expression2, expression3 }, innerTupleKeyGetter, innerTupleValueGetter)
+          },
+          outerTupleKeyGetter,
+          outerTupleValueGetter);
+
+      var result = TupleExpressionBuilder.GetExpressionsFromTuple (tupleExpression);
+
+      var enumerator = result.GetEnumerator();
+      enumerator.MoveNext();
+      ExpressionTreeComparer.CheckAreEqualTrees (
+          enumerator.Current, MemberExpression.MakeMemberAccess (tupleExpression, outerTupleCtor.DeclaringType.GetProperty ("Key")));
+      enumerator.MoveNext();
+      ExpressionTreeComparer.CheckAreEqualTrees (
+          enumerator.Current,
+          MemberExpression.MakeMemberAccess (
+              MemberExpression.MakeMemberAccess (tupleExpression, outerTupleCtor.DeclaringType.GetProperty ("Value")),
+              innerTupleCtor.DeclaringType.GetProperty ("Key")));
+      enumerator.MoveNext();
+      ExpressionTreeComparer.CheckAreEqualTrees (
+          enumerator.Current,
+          MemberExpression.MakeMemberAccess (
+              MemberExpression.MakeMemberAccess (tupleExpression, outerTupleCtor.DeclaringType.GetProperty ("Value")),
+              innerTupleCtor.DeclaringType.GetProperty ("Value")));
+    }
+  }
+}
---
+++ Remotion/Data/Linq/Parsing/TupleExpressionBuilder.cs
@@ -0,0 +1,67 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Remotion.Data.Linq.Parsing
+{
+  /// <summary>
+  /// <see cref="TupleExpressionBuilder"/> can be used to build tuples incorporating a sequence of <see cref="Expression"/>s. 
+  /// For example, given three expressions, exp1, exp2, and exp3, it will build nested <see cref="NewExpression"/>s that are equivalent to the 
+  /// following: new KeyValuePair(exp1, new KeyValuePair(exp2, exp3)).
+  /// Given an <see cref="Expression"/> whose type matches that of a tuple built by <see cref="TupleExpressionBuilder"/>, the builder can also return 
+  /// an enumeration of accessor expressions that can be used to access the tuple elements in the same order as they were put into the nested tuple 
+  /// expression. In above example, this would yield tupleExpression.Key, tupleExpression.Value.Key, and tupleExpression.Value.Value.
+  /// This class can be handy whenever a set of <see cref="Expression"/> needs to be put into a single <see cref="Expression"/> 
+  /// (eg., a select projection), especially if each sub-expression needs to be explicitly accessed at a later point of time (eg., to retrieve the 
+  /// items from a statement surrounding a sub-statement yielding the tuple in its select projection).
+  /// </summary>
+  public class TupleExpressionBuilder
+  {
+    public static Expression AggregateExpressionsIntoTuple (IEnumerable<Expression> expressions)
+    {
+      return expressions
+          .Reverse ()
+          .Aggregate (
+              (current, expression) => CreateTupleExpression (expression, current));
+    }
+
+    public static IEnumerable<Expression> GetExpressionsFromTuple (Expression tupleExpression)
+    {
+      while (tupleExpression.Type.IsGenericType && tupleExpression.Type.GetGenericTypeDefinition() == typeof (KeyValuePair<,>))
+      {
+        yield return Expression.MakeMemberAccess (tupleExpression, tupleExpression.Type.GetProperty ("Key"));
+        tupleExpression = Expression.MakeMemberAccess (tupleExpression, tupleExpression.Type.GetProperty ("Value"));
+      }
+
+      yield return tupleExpression;
+    }
+
+    private static Expression CreateTupleExpression (Expression left, Expression right)
+    {
+      var tupleType = typeof (KeyValuePair<,>).MakeGenericType (left.Type, right.Type);
+      var newTupleExpression =
+          Expression.New (
+              tupleType.GetConstructor (new[] { left.Type, right.Type }),
+              new[] { left, right },
+              new[] { tupleType.GetMethod ("get_Key"), tupleType.GetMethod ("get_Value") });
+      return newTupleExpression;
+    }
+  }
+}
