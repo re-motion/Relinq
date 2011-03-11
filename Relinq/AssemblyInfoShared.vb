a18b8b40664897bb250bb9999bdedfe53f99e73d
Michael Ketting <michael.ketting@rubicon.eu>
2011-03-11 15:08:09 +0000

    Added VB.NET integration test AssemblyInfoShared.vb

---
+++ Relinq/AssemblyInfoShared.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Security
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyConfiguration(".NET Framework: net-3.5, build type: Debug")> 
+<Assembly: AssemblyCompany("rubicon informationstechnologie gmbh")> 
+<Assembly: AssemblyProduct("re-motion core framework (www.re-motion.org)")> 
+<Assembly: AssemblyCopyright("(c) 2005 - 2011 rubicon informationstechnologie gmbh, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")> 
+<Assembly: AssemblyTrademark("")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.13.97.2")> 
+<Assembly: AssemblyFileVersion("1.13.97.2")> 
