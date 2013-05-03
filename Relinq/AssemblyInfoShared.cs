929b7ee57f1b1d3f8b549c5434f6f120845c5455
re-motion Build User <FX-Team@rubicon.eu>
2013-05-03 22:09:05 +0000

    Comitting AssemblyInfos for local build after setting version to '1.13.197.1'.

--- Relinq/AssemblyInfoShared.cs
+++ Relinq/AssemblyInfoShared.cs
@@ -28,7 +28,7 @@ using System.Reflection;
 [assembly: AssemblyProduct ("re-linq project (relinq.codeplex.com)")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.13.196.1")]
+[assembly: AssemblyInformationalVersion ("1.13.197.1")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -41,5 +41,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.13.196.1")]
-[assembly: AssemblyFileVersion ("1.13.196.1")]
+[assembly: AssemblyVersion("1.13.197.1")]
+[assembly: AssemblyFileVersion ("1.13.197.1")]
