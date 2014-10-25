f1f7f7f49f2da69f93d02c6fd1352c7b6073c170
Michael Ketting <michael.ketting@rubicon.eu>
2014-10-25 18:01:33 +0200

    RMLNQ-46 Imported build scripts via NuGet.

--- Build-Remotion.cmd
+++ Build-Remotion.cmd
@@ -1,6 +1,11 @@
 @echo off
 pushd %~dp0
 set msbuild="C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+set log-dir=Build\BuildOutput\log
+set nuget-bin=Build\BuildOutput\temp\nuget-bin
+set nuget=%nuget-bin%\nuget.exe
+set nuget-download=powershell.exe -NoProfile -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://www.nuget.org/nuget.exe','%nuget%')}"
+
 if not exist remotion.snk goto nosnk
 
 if not [%1]==[] goto %1
@@ -8,13 +13,12 @@ if not [%1]==[] goto %1
 echo Welcome to the re-motion build tool!
 echo.
 echo Choose your desired build:
-echo [1] ... Test build ^(x86-debug^)
-echo [2] ... Full build ^(x86-debug/release, x64-debug/release^)
-echo [3] ... Docs build ^(x86-debug if not present, docs^)
+echo [1] ... Test build ^(tests for x86-debug^)
+echo [2] ... Full build ^(tests for x64-debug/release; creates NuGet package^)
+echo [3] ... Docs build ^(only docs^)
 echo           Requires Sandcastle Help File Builder to be installed!
-echo [4] ... Package ^(create zip package from pre-existent build^)
-echo [5] ... Run DependDB
-echo [6] ... Oops, nothing please - exit.
+echo [4] ... Package ^(creates NuGet package^)
+echo [5] ... Oops, nothing please - exit.
 echo.
 
 choice /c:123456 /n /m "Your choice: "
@@ -23,37 +27,42 @@ if %ERRORLEVEL%==1 goto run_test_build
 if %ERRORLEVEL%==2 goto run_full_build
 if %ERRORLEVEL%==3 goto run_docs_build
 if %ERRORLEVEL%==4 goto run_pkg_build
-if %ERRORLEVEL%==5 goto run_dependdb
-if %ERRORLEVEL%==6 goto run_exit
+if %ERRORLEVEL%==5 goto run_exit
 goto build_succeeded
 
 :run_test_build
-mkdir build\BuildOutput\log
+mkdir %log-dir%
+mkdir %nuget-bin%
+%nuget-download%
+%nuget% restore . -NonInteractive
 %msbuild% build\Remotion.Local.build /t:TestBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
 :run_full_build
-mkdir build\BuildOutput\log
-%msbuild% build\Remotion.Local.build /t:FullBuild /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
+mkdir %log-dir%
+mkdir %nuget-bin%
+%nuget-download%
+%nuget% restore . -NonInteractive
+%msbuild% build\Remotion.Local.build /t:FullBuildWithoutDocumentation /maxcpucount /verbosity:normal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
 :run_docs_build
-mkdir build\BuildOutput\log
-%msbuild% build\Remotion.Local.build /t:DocumentationTestBuild /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
+mkdir %log-dir%
+mkdir %nuget-bin%
+%nuget-download%
+%nuget% restore . -NonInteractive
+%msbuild% build\Remotion.Local.build /t:DocumentationBuild /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
 :run_pkg_build
-mkdir build\BuildOutput\log
-%msbuild% build\Remotion.Local.build /t:ZipPackageAll /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log "/p:Version=0.0.0.0"
-if not %ERRORLEVEL%==0 goto build_failed
-goto build_succeeded
-
-:run_dependdb
-mkdir build\BuildOutput\log
-%msbuild% build\Remotion.build /t:DependDB /maxcpucount /verbosity:normal /flp:verbosity=detailed;logfile=build\BuildOutput\log\build.log "/p:Version=0.0.0.0;DependDBProjectBranch=Trunk;DependDBProjectVersion=0.0.0.0;DependDBProjectVCSUrlTemplate=https://svn.re-motion.org:443/svn/Remotion/!svn/bc/$(DependDBProjectRevision)/trunk/{0};DependDBProjectImportNotificationMailAddress=%USERNAME%@rubicon.eu;DependDBUploadPath=C:\Temp\LocalDependDBUploadDirectory"
+mkdir %log-dir%
+mkdir %nuget-bin%
+%nuget-download%
+%nuget% restore . -NonInteractive
+%msbuild% build\Remotion.Local.build /t:PackageBuild /maxcpucount /verbosity:minimal /flp:verbosity=normal;logfile=build\BuildOutput\log\build.log
 if not %ERRORLEVEL%==0 goto build_failed
 goto build_succeeded
 
