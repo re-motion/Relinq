3c28eb323868df347254ff4d735e3e78fb989c09
Michael Ketting <michael.ketting@rubicon.eu>
2015-03-22 15:27:58 +0100

    RMLNQ-41 Drop obsolete code from performance test.

--- PerformanceTests/Program.cs
+++ PerformanceTests/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using Remotion.Linq.Collections;
 using Remotion.Linq.Parsing.Structure;
 
 namespace Remotion.Linq.PerformanceTests
@@ -33,13 +29,6 @@ namespace Remotion.Linq.PerformanceTests
           Console.WriteLine ("Valid test types are: 'QueryParser'");
         return -1;
       }
-      //Only enable one of the methods.
-
-      //CreateDefaultNodeProvider();
-
-      //MethodsViaList();
-
-      //MethodsViaLookup();
     }
 
     private static void CreateDefaultQueryParser ()
@@ -58,46 +47,6 @@ namespace Remotion.Linq.PerformanceTests
       ExpressionTreeParser.CreateDefaultNodeTypeProvider();
       stopwatch.Stop();
       Console.WriteLine ("Time taken: {0}ms, reference time: ~72ms", stopwatch.ElapsedMilliseconds);
-    }
-
-    private static void MethodsViaList ()
-    {
-      var stopwatch1 = Stopwatch.StartNew();
-      var enumerable = typeof (Enumerable).GetMethods().ToLookup (mi => mi.Name);
-      var queryable = typeof (Enumerable).GetMethods().ToLookup (mi => mi.Name);
-      stopwatch1.Stop();
-      Console.WriteLine ("Time taken method reads: {0}µs", stopwatch1.Elapsed.TotalMilliseconds * 1000);
-
-      var result = new List<MethodInfo>();
-      var stopwatch2 = Stopwatch.StartNew();
-      foreach (MethodInfo methodInfo in enumerable.Concat (queryable))
-      {
-        if (methodInfo.Name == "Test")
-          result.Add (methodInfo);
-      }
-      stopwatch2.Stop();
-      Console.WriteLine ("Time taken single run: {0}µs", stopwatch2.Elapsed.TotalMilliseconds * 1000);
-    }
-
-    private static void MethodsViaLookup ()
-    {
-      var stopwatch1 = Stopwatch.StartNew();
-      var enumerable = typeof (Enumerable).GetMethods().ToLookup (mi => mi.Name);
-      var queryable = typeof (Enumerable).GetMethods().ToLookup (mi => mi.Name);
-      stopwatch1.Stop();
-      Console.WriteLine ("Time taken method reads: {0}µs", stopwatch1.Elapsed.TotalMilliseconds * 1000);
-
-      var result = new List<MethodInfo>();
-      var stopwatch2 = Stopwatch.StartNew();
-      for (int i = 0; i < 50; i++)
-      {
-        foreach (MethodInfo methodInfo in enumerable["Test"])
-          result.Add (methodInfo);
-        foreach (MethodInfo methodInfo in queryable["Test"])
-          result.Add (methodInfo);
-      }
-      stopwatch2.Stop();
-      Console.WriteLine ("Time taken 50 runs: {0}µs", stopwatch2.Elapsed.TotalMilliseconds * 1000);
     }
   }
 }
