a4aa5961a8318c839c3cf701a2e310d6b68fffb9
Michael Ketting <michael.ketting@rubicon.eu>
2015-06-27 20:15:26 +0200

    RMLNQ-91 Introduce IFromClause interface.

--- Core/Clauses/FromClauseBase.cs
+++ Core/Clauses/FromClauseBase.cs
@@ -26,10 +26,10 @@ using Remotion.Utilities;
 namespace Remotion.Linq.Clauses
 {
   /// <summary>
-  /// Base class for from clauses (<see cref="AdditionalFromClause"/> and <see cref="MainFromClause"/>). From clauses define query sources that
-  /// provide data items to the query which are filtered, ordered, projected, or otherwise processed by the following clauses.
+  /// Base class for <see cref="AdditionalFromClause"/> and <see cref="MainFromClause"/>.
   /// </summary>
-  public abstract class FromClauseBase : IClause, IQuerySource
+  /// <seealso cref="IFromClause"/>
+  public abstract class FromClauseBase : IFromClause
   {
     private string _itemName;
     private Type _itemType;
